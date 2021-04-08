f7bc7bf91135d5d4f932f7f68b9dfbe944f8a09d
Maarten van Gompel <proycon@anaproy.nl>
2021-04-08 23:52:58 +0200

    added simple dehyphenization script

---
+++ scripts/dehyphenize.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""Dehyphenize a text, joins lines that end in a hyphen"""
+
+import sys
+
+HYPHENS = ("¬","-")
+
+buffer = ""
+for line in sys.stdin:
+    line = line.strip()
+    if line and line[-1] in HYPHENS:
+        buffer += line[:-1]
+    elif buffer:
+        buffer += line
+        print(buffer)
+        buffer = ""
+    else:
+        print(line)
+
+if buffer:
+    print(buffer)

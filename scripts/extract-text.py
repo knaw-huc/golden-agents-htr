955b8275f5aff515a72c4e560f3a3a0e6bebd921
Maarten van Gompel <proycon@anaproy.nl>
2021-04-08 23:33:35 +0200

    added simple script to extract plain text from Page XML

---
+++ scripts/extract-text.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+import lxml.etree
+
+parser = argparse.ArgumentParser(description="Extract text contents from Page XML", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("files", nargs='*', help="Files")
+args = parser.parse_args()
+
+for filename in args.files:
+    doc = lxml.etree.parse(filename).getroot()
+    for element in doc.xpath("//pagexml:TextRegion/pagexml:TextLine/pagexml:TextEquiv/pagexml:Unicode",namespaces={'pagexml': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15' }):
+        print(element.text)

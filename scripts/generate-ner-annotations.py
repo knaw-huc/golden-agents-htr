cb3f6a883622c36cd66a4729ac806a5719ca42c6
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2021-10-20 23:19:00 +0200

    move code from ga-ner-experiments repository

---
+++ scripts/generate-ner-annotations.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import argparse
+import json
+
+from pagexml.parser import parse_pagexml_file
+
+from golden_agents.ner import NER
+
+
+def create_scan_id(file) -> str:
+    path_parts = file.split('/')
+    archive_id = path_parts[-2]
+    scan_id = path_parts[-1].replace('.xml', '')
+    return f"urn:golden-agents:{archive_id}:scan={scan_id}"
+
+
+def process_pagexml(file: str) -> list:
+    scan = parse_pagexml_file(file)
+    if not scan.id:
+        scan.id = create_scan_id(file)
+    scan.transkribus_uri = "https://files.transkribus.eu/iiif/2/MOQMINPXXPUTISCRFIRKIOIX/full/max/0/default.jpg"
+    ner = NER()
+    return ner.create_web_annotations(scan, "http://localhost:8080/textrepo/versions/x")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Perform NER on a PageXML file using Analiticcl, export resulst as Web Annotations",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("pagexmlfile",
+                        help="The PageXML file to extract NER annotations from",
+                        type=str)
+    args = parser.parse_args()
+
+    annotations = process_pagexml(args.pagexmlfile)
+    print(json.dumps(annotations,indent=4))
+
+
+if __name__ == '__main__':
+    main()

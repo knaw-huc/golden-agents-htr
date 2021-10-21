0bd5fa86a187b1c8bb57e51ae366dfa4455d3556
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2021-10-21 09:09:31 +0200

    check args

--- scripts/generate-ner-annotations.py
+++ scripts/generate-ner-annotations.py
@@ -2,9 +2,9 @@
 import argparse
 import json
 
-from pagexml.parser import parse_pagexml_file
-
 from golden_agents.ner import NER
+
+from pagexml.parser import parse_pagexml_file
 
 
 def create_scan_id(file) -> str:
@@ -19,8 +19,7 @@ def process_pagexml(file: str) -> list:
     if not scan.id:
         scan.id = create_scan_id(file)
     scan.transkribus_uri = "https://files.transkribus.eu/iiif/2/MOQMINPXXPUTISCRFIRKIOIX/full/max/0/default.jpg"
-    ner = NER()
-    return ner.create_web_annotations(scan, "http://localhost:8080/textrepo/versions/x")
+    return NER().create_web_annotations(scan, "http://localhost:8080/textrepo/versions/x")
 
 
 def main():
@@ -32,8 +31,9 @@ def main():
                         type=str)
     args = parser.parse_args()
 
-    annotations = process_pagexml(args.pagexmlfile)
-    print(json.dumps(annotations,indent=4))
+    if args.pagexmlfile:
+        annotations = process_pagexml(args.pagexmlfile)
+        print(json.dumps(annotations, indent=4))
 
 
 if __name__ == '__main__':

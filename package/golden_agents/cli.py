bb61ce5238dc5969b6470d2b98b622364d58cfa4
Maarten van Gompel <proycon@anaproy.nl>
2021-10-21 11:31:09 +0200

    added support for operating on multiple pagexml files in sequence (without reinstantiating the model)

--- package/golden_agents/cli.py
+++ package/golden_agents/cli.py
@@ -8,16 +8,18 @@ def main():
     parser = argparse.ArgumentParser(
         description="Perform NER on a PageXML file using Analiticcl, export results as Web Annotations",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("pagexmlfile",
-                        help="The PageXML file to extract NER annotations from",
+    parser.add_argument("pagexmlfiles",
+                        nargs="*",
+                        help="The PageXML file(s) to extract NER annotations from",
                         type=str)
     args = parser.parse_args()
 
     ner = NER()
 
-    if args.pagexmlfile:
-        annotations = ner.process_pagexml(args.pagexmlfile)
-        print(json.dumps(annotations, indent=4))
+    if args.pagexmlfiles:
+        for pagexmlfile in args.pagexmlfiles:
+            annotations = ner.process_pagexml(pagexmlfile)
+            print(json.dumps(annotations, indent=4))
 
 
 if __name__ == '__main__':

6be0c9c47024cdae1ff91ef0b5a0f734aaa426b3
Maarten van Gompel <proycon@anaproy.nl>
2021-10-21 11:08:21 +0200

    moved CLI interface into package

--- package/setup.py
+++ package/setup.py
@@ -10,15 +10,21 @@ setup(
     name='golden_agents',
     version='0.0.1',
 
-    description='Utility functions for thwe Golden Agents project',
+    description='NER pipeline and utility functions for the Golden Agents project',
     long_description=readme,
     long_description_content_type='text/markdown',
 
     author='Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>',
     author_email='bram.buitendijk@di.huc.knaw.nl',
 
-    url='https://github.com/knaw-huc/ga-ner-experiment',
+    url='https://github.com/knaw-huc/golden-agents-htr/package',
     license='MIT',
 
     packages=find_packages(exclude=('tests', 'docs')),
+
+    entry_points={
+        'console_scripts': [
+            'golden-agents-ner = golden_agents.cli:main'
+        ]
+    }
 )

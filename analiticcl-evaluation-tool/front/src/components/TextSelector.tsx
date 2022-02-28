b4b799f6636348a9d09e5272b93a6f08e0a09d95
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2022-02-28 13:43:44 +0100

    analiticcl-evaluation-tool: formatting

--- analiticcl-evaluation-tool/front/src/components/TextSelector.tsx
+++ analiticcl-evaluation-tool/front/src/components/TextSelector.tsx
@@ -1,21 +1,19 @@
 interface Props {
-  basenames: string[]
-  onChange: (value: string) => void
+  basenames: string[];
+  onChange: (value: string) => void;
 }
 
 export function TextSelector(props: Props) {
   return (
     <span>
       Text: &nbsp;
-      <select onChange={e => props.onChange(e.target.value)}>
-        {
-          props.basenames.map(option =>
-            <option key={option} value={option}>
-              {option}
-            </option>
-          )
-        }
+      <select onChange={(e) => props.onChange(e.target.value)}>
+        {props.basenames.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
       </select>
     </span>
-  )
+  );
 }
--- analiticcl-evaluation-tool/front/src/components/VersionSelector.tsx
+++ analiticcl-evaluation-tool/front/src/components/VersionSelector.tsx
@@ -1,21 +1,19 @@
 interface Props {
-  versions: string[]
-  onChange: (value: string) => void
+  versions: string[];
+  onChange: (value: string) => void;
 }
-  
+
 export function VersionSelector(props: Props) {
   return (
     <span>
       Version: &nbsp;
       <select onChange={(e) => props.onChange(e.target.value)}>
-        {
-          props.versions.map(option =>
-            <option key={option} value={option}>
-              {option}
-            </option>
-          )
-        }
+        {props.versions.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
       </select>
     </span>
-  )
+  );
 }

7c0ad306da0de92d0edc59df3279dc656dc760ef
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2022-08-15 16:21:55 +0200

    add namespaces + default vocabulary option

--- scripts/jsonld-to-graph.py
+++ scripts/jsonld-to-graph.py
@@ -2,8 +2,9 @@
 
 import argparse
 import gzip
+import json
 import sys
-from typing import List
+from typing import List, Union
 
 from rdflib import Graph, URIRef
 
@@ -24,18 +25,22 @@ def main():
     parser.add_argument('-f', '--format',
                         help=f"set the export format (options: {sorted_formats})",
                         action='store', required=False, default='turtle')
+    parser.add_argument('-v', '--vocab', help="add default vocabulary", action='store', required=False)
 
     args = parser.parse_args()
     if args.format not in export_formats:
         raise Exception(f"format '{args.format}' not recognized: use one of {sorted_formats}")
     if args.jsonld_file:
-        export_graph(args.jsonld_file, args.format, args.compress)
+        if args.vocab:
+            export_graph(args.jsonld_file, args.format, args.compress, args.vocab)
+        else:
+            export_graph(args.jsonld_file, args.format, args.compress)
     else:
         parser.print_usage()
 
 
-def export_graph(input_files, output_format, compress):
-    graph = setup_graph(input_files)
+def export_graph(input_files, output_format, compress, vocab=None):
+    graph = setup_graph(input_files, vocab)
     out = graph.serialize(format=output_format)
     if compress:
         sys.stdout.buffer.write(gzip.compress(bytes(out, 'utf-8')))
@@ -43,16 +48,40 @@ def export_graph(input_files, output_format, compress):
         print(out)
 
 
-def setup_graph(jsonld_files: List[str]):
+def setup_graph(jsonld_files: List[str], vocab: Union[str, None] = None):
     g = Graph()
-    g.namespace_manager.bind("oa", URIRef("http://www.w3.org/ns/oa#"))
-    g.namespace_manager.bind("as", URIRef("http://www.w3.org/ns/activitystreams#"))
     g.namespace_manager.bind("ana", URIRef("http://purl.org/analiticcl/terms#"))
+    g.namespace_manager.bind("as", URIRef("http://www.w3.org/ns/activitystreams#"))
+    g.namespace_manager.bind("dct", URIRef("http://purl.org/dc/terms/"))
+    g.namespace_manager.bind("foaf", URIRef("http://xmlns.com/foaf/0.1/"))
+    g.namespace_manager.bind("oa", URIRef("http://www.w3.org/ns/oa#"))
+    g.namespace_manager.bind("rpp", URIRef("https://data.goldenagents.org/ontology/rpp/"))
+    g.namespace_manager.bind("vm", URIRef("https://humanities.knaw.nl/ns/variant-matching#"))
+
     for jf in jsonld_files:
         with open(jf) as f:
             jsonld = f.read()
+        if vocab:
+            g.namespace_manager.bind("", URIRef(vocab))
+            jsonld = add_default_vocabulary_to_context(vocab, json.loads(jsonld))
         g.parse(data=jsonld, format='json-ld')
     return g
+
+
+def add_default_vocabulary_to_context(vocab, annotations):
+    new_annotations = list()
+    for a in annotations:
+        context = a.get("@context")
+        context_list = []
+        if isinstance(context, str):
+            context_list.append(context)
+        else:
+            context_list.extend(context)
+        context_list.append({"@vocab": vocab})
+        a["@context"] = context_list
+        new_annotations.append(a)
+    jsonld = json.dumps(new_annotations)
+    return jsonld
 
 
 if __name__ == '__main__':

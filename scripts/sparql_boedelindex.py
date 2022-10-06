a86df9b08f3ab2efeccb4291cce7a8e85399d754
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2022-10-06 13:27:23 +0200

    include inventoryNumber in boedelindex.csv

--- scripts/sparql_boedelindex.py
+++ scripts/sparql_boedelindex.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 
 import sys
+
 from sparql_common import query
 
 if __name__ == "__main__":
     URL = "https://sparql.goldenagents.org/"
 
     q = """
+    PREFIX dcterms: <http://purl.org/dc/terms/>
     PREFIX schema: <http://schema.org/>
     PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
     PREFIX roar: <https://data.goldenagents.org/ontology/roar/>
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    SELECT DISTINCT ?akteIndex ?eventTypeLabel ?scanNaam WHERE {
+    SELECT DISTINCT ?akteIndex ?eventTypeLabel ?scanNaam ?inventoryNumber WHERE {
       GRAPH <https://data.goldenagents.org/datasets/u692bc364e9d7fa97b3510c6c0c8f2bb9a0e5123b/na_20210715> {
         
         # Index op een akte (URI = Stadsarchief)
@@ -31,11 +33,18 @@ if __name__ == "__main__":
         ?akte a <https://data.goldenagents.org/thesaurus/NotarieleArchieven> ;
               roar:createdAt ?date ;
 #          roar:createdBy ?notaris ;
-              roar:hasScan ?scan .
+              roar:hasScan ?scan ;
+              roar:partOf ?inventory .
         
         # Scan heeft een label (naam = bestandsnaam)
         ?scan a roar:Scan ;
               rdfs:label ?scanNaam .
+              
+        ?inventory a roar:InventoryBook .
+        
+        ?inventoryIndex roar:indexOf ?inventory ;
+                        dcterms:identifier ?inventoryNumber ;
+                        rdfs:label ?inventoryName .
       
         # Alleen boedelscheidingen en boedelinventarissen
         FILTER(REGEX(?eventTypeLabel, 'boedel', 'i'))
